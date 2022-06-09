0bbe3bad5378d8eb2810e2d5c1b2c0f17297c16b
bollilia <bollykevin75@gmail.com>
2022-06-09 05:25:59 -0700

    Multiple mains

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - used to check the condition
+ *
+ * Description: return the result of the function
+ *
+ * Return: 0
+ *
+ */
+
+void positive_or_negative(int c)
+{
+
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	} else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	} else
+	{
+		printf("%d is negative\n", n);
+	}
+
+	return (0);
+}
